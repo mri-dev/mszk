ab166c982d16cbfd9b4be728fa98d6826683c00c
mri-dev <pocokxtrame@gmail.com>
2019-06-19 16:33:24 +0200

    Save

---
+++ cp/application/views/v1.0/dokumentumok/index.php
@@ -0,0 +1,32 @@
+<div class="documents-controll">
+  <div class="wblock">
+    <div class="data-container">
+      <div class="d-flex">
+        <div class="sidebar">
+          <div class="head">
+            <i class="fas fa-folder"></i> <?=__('Dokumentum mappák')?>
+          </div>
+          <div class="folders">
+
+          </div>
+        </div>
+        <div class="docs">
+          <div class="head">
+            <?=__('Összes dokumentum')?>
+          </div>
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th><?=__('Cím')?></th>
+                  <th><?=__('Mappa')?></th>
+                  <th><?=__('Létrehozva')?></th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
